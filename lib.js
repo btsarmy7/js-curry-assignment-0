302e6b284161e36acfbcb68c511291f83cc9328c
Xiaodan Yu <yuxxx827@umn.edu>
2017-10-26 19:50:00 -0700

    commit 1

--- lib.js
+++ lib.js
@@ -1,5 +1,7 @@
 'use strict'
 
+
+
 const entries =
   obj =>
     Object.keys(obj)
@@ -23,15 +25,44 @@ const cart =
     items
   })
 
+  const listedPrice =
+  listing =>
+  name =>
+    name === listing.name
+      ? listing.price
+      : 0
+
+      const listings = [
+        listing('detergent', 10),
+        listing('hennessey', 100),
+        listing('carlo rozzi', 20),
+        listing('coffee', 5),
+        listing('cookies', 6),
+        listing('mountain dew', 2)
+      ]
+     
+
+  const itemPrice = (listingArray, Item) => {
+    let a = listingArray.map((i) => listing(Item, listedPrice(i)(Item)))
+    return (a.map((p) => p = p.price)).reduce((total, next) => total + next, 0)    
+}
+
+
 /**
  * should return an array with the `itemName` repeated `count` number of times
+ * ['carlo rossi', 'carlo rossi' ...6x]
  */
 const itemRepeater =
   itemName =>
-    count => {
-      // TODO
+    count => { 
+        let i = 1
+        let array = []
+        while (i++ <= count){
+        array.push(({name:itemName, price: itemPrice(listings, itemName)}))      
+        }
+      return array    
     }
-
+   
 /**
  * should return an array of carts with each given customer's shopping list
  * as an array of items
@@ -39,11 +70,37 @@ const itemRepeater =
 const constructCarts =
   listings =>
     customers => {
-      // TODO
+      return customers.map((c) => ({name: c.name, list: entries(c.shoppingList).map( (i) => ({itemName: i[0], count: i[1]}))}))
+                  .map( (element) => ({name: element.name, cart: element.list.map((itm) => itemRepeater(itm.itemName)(itm.count))}) )
     }
 
+  
+    
+
+    const customers = [
+      customer('adam', {
+        'carlo rozzi': 6
+      }),
+      customer('david', {
+        'detergent': 1,
+        'hennessey': 1,
+        'coffee': 1
+      }),
+      customer('michael', {
+        'coffee': 3,
+        'hennessey': 3
+      }),
+      customer('dillon', {
+        'cookies': 3,
+        'mountain dew': 3
+      })
+    ]
+    
+   
+    
+
 module.exports = {
-  listing,
+  listing, 
   customer,
-  constructCarts
+  constructCarts 
 }
--- main.js
+++ main.js
@@ -22,19 +22,29 @@ const customers = [
     'carlo rozzi': 6
   }),
   customer('david', {
-    detergent: 1,
-    hennessey: 1,
-    coffee: 1
+    'detergent': 1,
+    'hennessey': 1,
+    'coffee': 1
   }),
   customer('michael', {
-    coffee: 3,
-    hennessey: 3
+    'coffee': 3,
+    'hennessey': 3
   }),
   customer('dillon', {
-    cookies: 3,
+    'cookies': 3,
     'mountain dew': 3
   })
 ]
+
+//console.log(entries(customers[0].shoppingList))
+    //console.log(customers.map((c) => ({name: c.name, itemName: entries(c.shoppingList)[0], count: entries(c.shoppingList)[1]})))
+    //let array = customers.map((c) => ({name: c.name, list: entries(c.shoppingList).map( (i) => ({itemName: i[0], count: i[1]}))}))
+    //let arrayR = array.map( (element) => ({name: element.name, cart: element.list.map((itm) => itemRepeater(itm.itemName)(itm.count))}) )
+    //console.log(...arrayR)
+   //console.log(...arrayR)
+
+    console.log(constructCarts(listings)(customers))
+
 
 const carts = constructCarts(listings)
 
